00e85b134c010affd232397a87dfa1f811b487c0
Yetnayet Abebe <101176690+YetuAb@users.noreply.github.com>
2024-07-19 08:00:18 +0300

    Modified it so that it routes from the login page to the dashboard when authentication is performed and performs additional functionalities

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import ProductDisplayPage from "./pages/ProductDisplayPage";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header, SubNavigation } from "./components/Header";
 import SocialMediaPage from "./pages/SocialMediaPage";
+import ProductDisplayPage from "./pages/ProductDisplayPage";
+import Login from "./pages/Login";
+import ProductDetail from "./pages/ProductDetail";
 
-const isAuthenticated = () => {
-  return true;
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("EXPERIMENTAL");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <Router>
+      <div
+        className={`min-h-screen ${
+          darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-black"
+        }`}
+      >
+        {isAuthenticated ? (
+          <>
+            <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <SubNavigation
+              selectedCategory={selectedCategory}
+              onCategorySelect={handleCategorySelect}
+            />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  selectedCategory === "DIGITAL" ? (
+                    <SocialMediaPage selectedCategory={selectedCategory} />
+                  ) : selectedCategory === "CREATIVE" ? (
+                    <ProductDisplayPage selectedCategory={selectedCategory} />
+                  ) : (
+                    <div className="p-4">Content.</div>
+                  )
+                }
+              />
+              <Route path="/details" element={<ProductDetail />} />
+            </Routes>
+          </>
+        ) : (
+          <Login onLogin={handleLogin} />
+        )}
+      </div>
+    </Router>
+  );
 };
-
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/products" />} />
-      <Route path="/products" element={<ProductDisplayPage />} />
-      <Route
-        path="/social"
-        element={isAuthenticated() ? <SocialMediaPage /> : <Navigate to="/" />}
-      />
-    </Routes>
-  </Router>
-);
 
 export default App;
